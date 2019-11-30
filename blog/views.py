52b1feb094f5507a62380bafba573f2c2ea22e58
Cam Deatherage <ctd1077@gmail.com>
2019-11-30 12:34:35 -0700

    Updated test

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -32,3 +33,11 @@ class BlogUpdateView(UpdateView):
     template_name = 'post_edit.html'
 
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    
+    model = Post
+
+    template_name = 'post_delete.html'
+
+    success_url = reverse_lazy('home')
